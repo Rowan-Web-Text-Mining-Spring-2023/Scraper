ddde5e0678a5b9804c9f8e24a89545869f953275
Vince <45806875+vince1444@users.noreply.github.com>
2023-01-26 23:15:37 -0500

    Finished tweet fetching functionality

--- scraper.py
+++ scraper.py
@@ -1,20 +1,13 @@
-import nltk, tweepy, yaml
-from Data.Input.twitter_handles import twitter_handles
-from Functions.create_authentication import auth as tweepy_auth
+import nltk
+from Data.Input.twitter_users import twitter_users as users
+from Functions.add_to_dictionary import add_to_dictionary
+from Functions.fetch_tweets_by_user import fetch_tweets_by_user
 
-NUMBER_OF_TWEETS = 10
 
 if __name__ == "__main__":
-    pass
+    aggregate_dictionary = {}
 
-    # api = tweepy.API(auth)
+    for user in users:
+        add_to_dictionary(aggregate_dictionary, fetch_tweets_by_user(user))
 
-    # public_tweets = api.home_timeline()
-
-    # user = api.get_user(screen_name=twitter_handles[0])
-
-    # tweets = api.user_timeline(screen_name=twitter_handles[0], count=NUMBER_OF_TWEETS)
-
-    # for tweet in tweets[:NUMBER_OF_TWEETS]:
-    #     print(tweet.text)
-    #     print("==========")
+    print(aggregate_dictionary)
